f2c001b9e7d4f34ea6a66aaa20519685073ad657
grzesiek <gregorius2514@gmail.com>
2019-02-06 20:54:06 +0100

    Find pom version

--- src/main/kotlin/MavenVersionUpdater.kt
+++ src/main/kotlin/MavenVersionUpdater.kt
@@ -2,25 +2,63 @@ import org.w3c.dom.NodeList
 import org.xml.sax.InputSource
 import java.io.File
 import java.io.StringReader
+import java.lang.RuntimeException
+import java.util.regex.Pattern
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.xpath.XPathConstants
 import javax.xml.xpath.XPathFactory
+import javax.xml.transform.stream.StreamResult
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.TransformerFactory
 
 fun main(args: Array<String>) {
+   
+    if (args.size != 1) {
+       throw RuntimeException("Required one argument with maven project path") 
+    }
+    
+    File(args[0]).walk().forEach { file ->
+        if ("pom.xml".equals(file.name))
+            println(file)
+    }
+
     val xmlFile = File("src/main/resources/pom.xml")
 
-    val dbFactory = DocumentBuilderFactory.newInstance()
-    val dBuilder = dbFactory.newDocumentBuilder()
-    val xmlInput = InputSource(StringReader(xmlFile.readText()))
-    val doc = dBuilder.parse(xmlInput) 
-    
-    
-    val xpFactory = XPathFactory.newInstance()
-    val xPath = xpFactory.newXPath()
+    val domXmlFactory = DocumentBuilderFactory.newInstance()
+    val domXmlBuilder = domXmlFactory.newDocumentBuilder()
+    val xmlInputStream = InputSource(StringReader(xmlFile.readText()))
+    val xmlDom = domXmlBuilder.parse(xmlInputStream)
 
-    val xpath = "//version"
 
-    val itemsTypeT1 = xPath.evaluate(xpath, doc, XPathConstants.NODESET) as NodeList
-    println("version number: " + itemsTypeT1.item(0).textContent))
+    val xpathFactory = XPathFactory.newInstance()
+    val xpath = xpathFactory.newXPath()
+
+    val pomVersionRegex = "//version"
+    val pomVersionElement = xpath.evaluate(pomVersionRegex, xmlDom, XPathConstants.NODESET) as NodeList
+    val projectVersion = pomVersionElement.item(0).textContent
+
+    val searchVersionRegex = Pattern.compile("([0-9.]+)(-SNAPSHOT)?")
+    val matcher = searchVersionRegex.matcher(projectVersion)
+
+    matcher.find()
+    val version = matcher.group(1)
+
+    if (version.contains(".")) {
+        val versions = version.split(".")
+        val mainorVersion = versions.get(versions.size - 1).toInt()
+
+        val nextVersion = version.replace(".$mainorVersion", ".${mainorVersion.inc()}")
+        val nextProjectVersion = projectVersion.replace(version, nextVersion)
+        println("next: $nextProjectVersion")
+
+        pomVersionElement.item(0).textContent = nextProjectVersion
+
+        val xformer = TransformerFactory.newInstance().newTransformer()
+        xformer.transform(DOMSource(xmlDom), StreamResult(xmlFile))
+    } else {
+        println("dots not detected")
+    }
+
+    println("version number: $projectVersion, version: $version")
 
 }
